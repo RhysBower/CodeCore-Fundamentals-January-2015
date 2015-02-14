e877a48f9cf935bf0f0baad8dba6247fd2836682
Rhys Bower <bowerrhys@gmail.com>
2015-02-14 12:58:48 -0800

    Add name map assignment.

--- week_2/map.rb
+++ week_2/map.rb
@@ -1,3 +1,18 @@
 my_array = [ 1, 2,3 ]
 
 puts my_array.map { |x| x * x }
+
+names_array = []
+
+while true
+	print "Give me a a name ('quit' to end input): "
+	name = gets.chomp
+	if name == "quit"
+		break
+	end
+	names_array.push(name)
+end
+
+names_array.map { |name| name.capitalize!.reverse! }
+
+puts names_array
