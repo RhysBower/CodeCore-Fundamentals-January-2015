d1205a9db361bb4a0a743c755dec426933b5b404
Rhys Bower <bowerrhys@gmail.com>
2015-02-14 12:39:20 -0800

    Multi-dimentional array print. The good way.

--- week_2/depth_array_print.rb
+++ week_2/depth_array_print.rb
@@ -1,7 +1,24 @@
 my_array = [ [1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3] ]
 
+puts "The bad way"
+
 my_array.each do |element|
 	element.each do |subelement|
 		puts subelement
 	end
 end
+
+
+puts "The good way"
+
+def print_array(array)
+	array.each do |element|
+		if element.kind_of?(Array)
+			print_array(element)
+		else
+			puts element
+		end
+	end
+end
+
+print_array(my_array)
