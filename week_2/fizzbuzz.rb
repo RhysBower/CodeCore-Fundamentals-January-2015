acec6d984d0cf29a12e8e1094f9b56c952170af0
Rhys Bower <bowerrhys@gmail.com>
2015-02-14 11:24:35 -0800

    Allow the user to pick the fizz and buzz words.

--- week_2/fizzbuzz.rb
+++ week_2/fizzbuzz.rb
@@ -1,12 +1,18 @@
+print "Give me a word for FIZZ: "
+fizz = gets.chomp
+
+print "Give me a word for BUZZ: "
+buzz = gets.chomp
+
 fizzbuzz_array = []
 
 for i in 1..100
 	if i % 3 == 0 && i % 5 == 0
-		fizzbuzz_array.push("FIZZBUZZ")
+		fizzbuzz_array.push(fizz + buzz)
 	elsif i % 3 == 0
-		fizzbuzz_array.push("FIZZ")
+		fizzbuzz_array.push(fizz)
 	elsif i % 5 == 0
-		fizzbuzz_array.push("BUZZ")
+		fizzbuzz_array.push(buzz)
 	else
 		fizzbuzz_array.push(i)
 	end
