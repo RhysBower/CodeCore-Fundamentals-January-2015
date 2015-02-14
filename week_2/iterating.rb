c85f1c98b80c53376f97b01f4fcf078fdd2914b3
Rhys Bower <bowerrhys@gmail.com>
2015-02-14 11:53:36 -0800

    Name array example.

--- week_2/iterating.rb
+++ week_2/iterating.rb
@@ -4,3 +4,22 @@ my_array.each do |number|
 	y = number ** 2
 	puts y
 end
+
+
+puts "Name exercise"
+
+names_array = [ "rhys", "bill", "jane", "sam", "janice" ]
+
+capitalized_name_array = []
+
+names_array.each do |name|
+	capitalized_name = name.capitalize
+
+	puts capitalized_name
+
+	capitalized_name_array.push(capitalized_name)
+end
+
+puts "Capitalized name array"
+
+puts capitalized_name_array
