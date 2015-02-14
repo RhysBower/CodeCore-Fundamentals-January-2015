b827c0745da2a2bce6a84276c26960053e683563
Rhys Bower <bowerrhys@gmail.com>
2015-02-14 10:48:47 -0800

    First array problem.

---
+++ week_2/array.rb
@@ -0,0 +1,13 @@
+my_array = [ [ "Hello", "World", "CodeCore" ], [ 2, 22, 2015 ], [ true, false ], nil ]
+
+puts "Array count"
+
+puts my_array.count
+
+puts "my_array"
+
+puts my_array
+
+puts "flatten array"
+
+puts my_array.length
