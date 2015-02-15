6c75be5ba67ff0226203611f1f01c9c000fb8a1e
Rhys Bower <bowerrhys@gmail.com>
2015-02-14 17:04:51 -0800

    Add most repeating character challenge.

--- week_2/method.rb
+++ week_2/method.rb
@@ -15,3 +15,40 @@ def array_to_hash(array)
 end
 
 puts array_to_hash( [ "hello", "greetings", "hola", "hi" ] )
+
+
+def most_reccurring_letter(string)
+	array = string.split("")
+
+	letter_hash = {}
+
+	array.each do |char|
+		if letter_hash[char.to_sym] == nil
+			letter_hash[char.to_sym] = 0
+		end
+		letter_hash[char.to_sym] = letter_hash[char.to_sym] + 1
+	end
+
+	greatestCount = 0
+	greatestLetter = ""
+
+	letter_hash.each do |letter, count|
+		if count > greatestCount
+			greatestCount = count
+			greatestLetter = letter
+		end
+	end
+
+	greatestLetter
+end
+
+puts "Give me a string and I will tell you what the most repeating letter is? "
+
+sentence = gets.chomp
+
+repeating_char =  most_reccurring_letter(sentence)
+
+puts "The most repeating character is #{repeating_char}"
+
+
+
