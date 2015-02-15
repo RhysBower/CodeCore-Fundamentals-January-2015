7a250052839849da562d1d1186ce62777a5a7249
Rhys Bower <bowerrhys@gmail.com>
2015-02-14 16:34:45 -0800

    Add array to hash example.

--- week_2/method.rb
+++ week_2/method.rb
@@ -3,3 +3,15 @@ def multiply( x, y)
 end
 
 puts multiply(3, 5)
+
+def array_to_hash(array)
+	hash = {}
+
+	array.each do |element|
+		hash[element.to_sym] = element.length()
+	end
+
+	hash
+end
+
+puts array_to_hash( [ "hello", "greetings", "hola", "hi" ] )
