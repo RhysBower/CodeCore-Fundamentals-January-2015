0b3e16106cc458551723ee05976e4b643fc874ee
Rhys Bower <bowerrhys@gmail.com>
2015-02-21 16:59:34 -0800

    Send some mail.

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "pony"
 
 get "/" do
   @name = params[:name]
@@ -7,4 +8,28 @@ end
 
 get "/about" do
   "This is my first Sinatra application"
+end
+
+get "/contact" do
+  erb :contact
+end
+
+post "/contact" do
+  Pony.mail({
+      to: "bowerrhys@gmail.com",
+      from: params[:email],
+      subject: "#{params[:name]} has a message",
+      body: params[:message],
+      via: :smtp,
+      via_options: {
+        address: "smtp.gmail.com",
+        port: "587",
+        enable_starttls_auto: true,
+        user_name: "answerawesome",
+        password: "Sup3r$ecret",
+        authentication: :plain,
+        domain: "localhost"
+      }
+    })
+  params.to_s
 end
